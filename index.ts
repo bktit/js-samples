319108e254959a7720954767b821fda7705f8796
bktit <bktit.develop@gmail.com>
2023-01-24 15:17:00 +0000

    coordenadas home cambiadas

--- index.ts
+++ index.ts
@@ -8,8 +8,8 @@ let map: google.maps.Map;
 
 function initMap(): void {
   map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: { lat: 43.261, lng: -2.943 },
+    zoom: 8, 
   });
 }
 
